cfa1c0f62f2b048b8fa40c2572a653d5574211d0
AntND <antonlenz508@gmail.com>
2025-07-28 19:24:18 +0200

    Update

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-
         int[][] gameField = initialize();
 
-
-
         playGame(gameField);
-
-
-
 
     }
 
 
 
     public static void playGame(int[][] gameField){
+
 
         printGameField(gameField);
 
@@ -31,38 +26,38 @@ public class Main {
             }
         }
 
-        int nachbarn;
+        int neighbours;
 
         for (int c = 0 ; c < gameField.length ; c++){
             for (int r = 0 ; r < gameField.length ; r++){
 
-                nachbarn = 0;
+                neighbours = 0;
 
                 for (int x = Math.max(c-1,0) ; x <= Math.min(c+1, gameField.length-1) ; x++){
                     for (int y = Math.max(r-1,0); y <= Math.min(r+1, gameField.length-1) ; y++){
 
-                        nachbarn += gameField[x][y];
+                        neighbours += gameField[x][y];
 
                     }
                 }
 
-                nachbarn -= gameField[c][r];
+                neighbours -= gameField[c][r];
 
                 if (gameField[c][r] == 1){
 
-                    if (nachbarn == 2 || nachbarn == 3){
+                    if (neighbours == 2 || neighbours == 3){
                         gameFieldUpdate[c][r] = 1;
                     }
-                    else if (nachbarn < 2){
+                    else if (neighbours < 2){
                         gameFieldUpdate[c][r] = 0;
                     }
-                    else if (nachbarn > 3){
+                    else if (neighbours > 3){
                         gameFieldUpdate[c][r] = 0;
                     }
                 }
                 else if (gameField[c][r] == 0){
 
-                    if (nachbarn == 3){
+                    if (neighbours == 3){
                         gameFieldUpdate[c][r] = 1;
                     }
                     else {
@@ -80,19 +75,14 @@ public class Main {
             }
         }
 
-        printGameField(gameField);
-
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Do you want to continue? y/n");
+        System.out.println("Continue? y/n");
         String answer = scanner.next();
 
         if (answer.toUpperCase().equals("Y")){
             playGame(gameField);
         }
     }
-
-
-
 
     public static void printGameField(int[][] gameField){
 
@@ -110,7 +100,6 @@ public class Main {
         string += "]";
 
         System.out.println(string);
-
 
     }
 
@@ -130,14 +119,7 @@ public class Main {
         gameField[1][4] = 1;
         gameField[2][4] = 1;
         gameField[3][4] = 1;
-
-
         return gameField;
 
-
     }
-
-
-
-
 }
